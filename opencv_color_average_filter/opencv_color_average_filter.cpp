3a4a9cf291e1cd37a8b5a821c1a7a9e73d209e8a
Thiều Quang Tuấn <tuancr261@gmail.com>
2017-03-11 20:50:05 +0700

    Average Filter on color image

--- opencv_color_average_filter/opencv_color_average_filter.cpp
+++ opencv_color_average_filter/opencv_color_average_filter.cpp
@@ -1,50 +1,46 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <cmath>
 using namespace cv;
 using namespace std;
-
+void averageFilter(Mat& input, Mat& output, vector<vector<double>>& kernel)
+{
+	double newRValue{}, newGValue{}, newBValue{};
+	int halfSize = kernel.size() / 2;
+	for (int i{ halfSize }; i < input.rows - halfSize; i++)
+		for (int j{ halfSize }; j < input.cols - halfSize; j++)
+		{
+			newRValue = newGValue = newBValue = 0;
+			for (int x = { -halfSize }; x <= halfSize; x++)
+				for (int y = { -halfSize }; y <= halfSize; y++)
+				{
+					newBValue += input.at<Vec3b>(i + x, j + y)[0] * kernel[x + halfSize][y + halfSize];
+					newGValue += input.at<Vec3b>(i + x, j + y)[1] * kernel[x + halfSize][y + halfSize];
+					newRValue += input.at<Vec3b>(i + x, j + y)[2] * kernel[x + halfSize][y + halfSize];
+				}
+			output.at<Vec3b>(i, j) = Vec3b(newBValue, newGValue, newRValue);
+		}
+}
 int main(int argc, char *argv[])
 {
-	Mat input_img, output_img;
+	Mat input_img;
 	input_img = imread(argv[1], IMREAD_COLOR);
 	if (input_img.empty())
 	{
 		cerr << "Couldn't open image . Exit program...................." << endl;
 		return 1;
 	}
-	output_img = input_img.clone();
+	Mat output_img(input_img.rows, input_img.cols, CV_8UC3);
 	//--------------------------------------------------------------------------------------------
-	vector<vector<Vec3b>> pixel(input_img.rows, vector<Vec3b>(input_img.cols));
-	Vec3b newPixel;
-	newPixel[0] = newPixel[1] = newPixel[2] = 0;
-	for (int i{}; i < input_img.rows; i++)
-		for (int j{}; j < input_img.cols; j++)
-		{
-			pixel[i][j] = input_img.at<Vec3b>(i, j);
-		}
-	for (int i{}; i < input_img.rows; i++)
-		for (int j{}; j < input_img.cols; j++)
-		{
-			if ((i == 0 || j == 0) || (i == input_img.rows - 1 || j == input_img.cols - 1)) continue;
-			newPixel[0] = newPixel[1] = newPixel[2] = 0;
-			for (int x = i - 1; x <= i + 1; x++)
-				for (int y = j - 1; y <= j + 1; y++)
-				{
-					newPixel[0] += pixel[x][y][0] / 9;
-					newPixel[1] += pixel[x][y][1] / 9;
-					newPixel[2] += pixel[x][y][2] / 9;
-				}
-			pixel[i][j] = newPixel;
-		}
-	for (int i{}; i < output_img.rows; i++)
-		for (int j{}; j < output_img.cols; j++)
-		{
-			output_img.at<Vec3b>(i, j) = pixel[i][j];
-		}
+	int kernelSize{};
+	cout << " Nhap vao kich thuoc m cua ma tran Kernel: ";
+	cin >> kernelSize;
+	vector<vector<double>> kernel(kernelSize, vector<double>(kernelSize, 1 / pow(kernelSize, 2.0)));
+	averageFilter(input_img, output_img, kernel);
 	namedWindow(argv[1], WINDOW_AUTOSIZE);
 	imshow(argv[1], input_img);
-	namedWindow("AfteraverageFilter.jpg", WINDOW_AUTOSIZE);
-	imshow("AfteraverageFilter.jpg", output_img);
+	namedWindow("average Filter", WINDOW_AUTOSIZE);
+	imshow("average Filter", output_img);
 
 	waitKey();
 	return 0;
